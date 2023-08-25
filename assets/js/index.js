975ebda29d317dd83436f1c3445c4495590d64cf
Marcos Bento <105390884+marcos-bento@users.noreply.github.com>
2023-08-25 09:07:52 -0300

    Ajuste de sintaxe de AddEventListener

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,13 +7,9 @@ const menu_image_bars = document.querySelector('.fa-bars');
 const close_menu_button = document.querySelector('.fa-x');
 const menu_links = document.querySelector('.hamb_menu')
 
-menu_button.addEventListener('click', () =>{
-	hambOpen();
-})
+menu_button.addEventListener('click', hambOpen());
 
-close_menu_button.addEventListener('click', () =>{
-	hambOpen();
-})
+close_menu_button.addEventListener('click', hambOpen());
 
 function hambOpen(){
     hambMenuShow(menu_links);
